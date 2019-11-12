e0abad6792323cea4ade8c0c13f5e264e1f4f478
vojsace <56891116+vojsace@users.noreply.github.com>
2019-11-13 00:06:49 +0100

    Add files via upload

---
+++ Ass2/Ass2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ass2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+
+            Console.Write("Input a number to check if its prime or NOT: ");
+            num = int.Parse(Console.ReadLine());
+
+
+                int i;
+                for (i = 2; i <= num - 1; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        Console.WriteLine("NOT PRIME");
+                        break;
+                    }
+                }
+                if (i == num)
+                {
+                    Console.WriteLine("ITS PRIME");
+                }
+            
+            Console.ReadKey();
+        }
+    }
+}
